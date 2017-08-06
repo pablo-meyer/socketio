4b8c56bb0292fc66b40c8254d3d2bb7efcc8962b
Pablo Meyer <pablomeyer@gmail.com>
2017-08-06 18:57:01 -0400

    Updated Development settings to use secure socket

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-    socketUrl : 'ws://psisocket-dev.azurewebsites.net',
-    secureSocket: false,
+    socketUrl : 'wss://psisocket-dev.azurewebsites.net',
+    secureSocket: true,
     socketAllowedOrigins:'http://psisocket-dev.azurewebsites.net',
     logEntriesToken: 'f7347545-b7ff-4682-894f-7473dac2c18c'
 };
